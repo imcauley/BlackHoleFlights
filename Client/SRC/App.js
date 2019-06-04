616a41364e37509bd37a5575fb414205b593f31f
Isaac McAuley <mcauleyisaac@gmail.com>
2019-06-04 15:03:03 -0600

    Added test form

--- Client/SRC/App.js
+++ Client/SRC/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+
+const API_URL = 'http://localhost:3000/'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <UserRow/>
+      <NameForm/> 
       </header>
     </div>
   );
@@ -23,7 +26,7 @@ class UserRow extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://localhost:3000/db_test/")
+    fetch(API_URL.concat('db_test/'))
       .then(res => res.json())
       .then(
         (result) => {
@@ -41,11 +44,51 @@ class UserRow extends React.Component {
       )
   }
 
-
   render() {
     return (<div className='UserRow'> {this.state.items} </div>);
   }
+}
 
+
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      submitted: false
+  };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    this.setState({submitted: true});
+    event.preventDefault();
+  }
+
+  render() {
+    if(!this.state.submitted) {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+    else {
+      return (
+        <div> Thank you {this.state.value} </div>
+      )
+    }
+  }
 }
 
 export default App;
