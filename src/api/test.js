8c115ff8fc9d17fa241c2020e48628f3fd8e094a
Isaac McAuley <mcauleyisaac@gmail.com>
2019-06-04 12:46:51 -0600

    Added database test

--- src/api/test.js
+++ src/api/test.js
@@ -1,6 +1,16 @@
 var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
+var mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "blackhole"
+});
+
+con.connect();
 
 app.listen(port);
 
@@ -8,5 +18,20 @@ app.get("/api_test", (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.json({"data": "this is test data"});
 });
+
+
+app.get("/db_test", (req, res, next) => {
+
+  con.query('SELECT * FROM User', function (error, results, fields) {
+    if (error) throw error;
+    data = results[0].username;
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.json({"first user": data});
+  });
+
+
+});
+
 
 console.log('todo list RESTful API server started on: ' + port);
